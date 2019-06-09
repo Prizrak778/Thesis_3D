1b14ee3d20ca16450df79eb0872228b629a3b3a4
prizrak778 <v.merkel778@gmail.com>
2019-06-09 23:04:54 +0700

    Добавлена функция создания проекции, заданы начальные параментры для камеры

--- Thesis_3D/Thesis_3D/Form1.cs
+++ Thesis_3D/Thesis_3D/Form1.cs
@@ -17,6 +17,25 @@ namespace Thesis_3D
 {
     public partial class Form1 : Form
     {
+        Camera camera1;
+        private Matrix4 _projectionMatrix;
+        private Matrix4 _modelView;
+        private Matrix4 _view;
+
+        private void CreateProjection()
+        {
+            float aspectRatio = (float)Width / Height;
+            _projectionMatrix = Matrix4.CreatePerspectiveFieldOfView(
+                90f * (float)Math.PI / 180f,
+                aspectRatio,
+                0.1f,
+                400f);
+            //_projectionMatrix = Matrix4.CreatePerspectiveOffCenter(-0.5f, 0.5f, -0.5f, 0.5f, 0.1f, 400f);
+            //_projectionMatrix = Matrix4.CreateOrthographicOffCenter(-4*glControl.Width / glControl.Height, 4*glControl.Width / glControl.Height, -4 / aspectRatio, 4 / aspectRatio, 0.7f, 400f);
+            //_projectionMatrix = Matrix4.CreateOrthographic(10* aspectRatio, 10, 0.1f, 400f );
+            _modelView = camera1.GetViewMatrix();
+            _view = _modelView * _projectionMatrix;
+        }
         public Form1()
         {
             OpenTK.Toolkit.Init();
@@ -52,6 +71,14 @@ namespace Thesis_3D
             base.OnClosing(e);
         }
         private void glControl_Load(object sender, EventArgs e)
+        {
+            CreateProjection();
+            camera1.Position = new Vector3(0, 2.5f, 2);
+            camera1.Orientation = new Vector3(-(float)Math.PI, -(float)Math.PI, 0);
+            // Ensure that the viewport and projection matrix are set correctly.
+            glControl_Resize(glControl1, EventArgs.Empty);
+        }
+        void glControl_Resize(object sender, EventArgs e)
         {
 
         }
